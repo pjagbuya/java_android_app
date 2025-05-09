a2056a99903f0332f6e47473188864c8aaeecb4f
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2025-05-09 10:38:22 +0800

    TEST : Final fixes plus one more warning for unversioned files please put them under gitignore after building

--- app/src/main/java/com/example/application_test/MainActivity.kt
+++ app/src/main/java/com/example/application_test/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.application_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG : String = "MAIN_ACTIVITY"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val t : Toast = Toast.makeText(
+            getApplicationContext(),
+            "Naalala mo pa ba yung PhilHealth issue",
+            Toast.LENGTH_LONG
+        )
+        t.show()
+        Log.d(TAG, "onCreate: end of function")
     }
 }
